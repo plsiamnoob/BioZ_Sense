f625c54683e9d16eb85d6d02b5d7c89ed37dd179
plsiamnoob <malihamza2006@gmail.com>
2026-09-09 18:23:18 -0700

    Removed more metric shit ton of redundant and unused libraries

--- deploy_windows_64.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set "MSYS2_PATH=C:\msys64"
-
-echo Starting Qt and UCRT64 dependency deployment...
-
-call "%MSYS2_PATH%\msys2_shell.cmd" -ucrt64 -here -defterm -no-start -shell bash -c "./deploy.sh"
-
-echo Deployment complete!
-pause
